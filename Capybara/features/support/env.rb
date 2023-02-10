58eed76f179ead8bdecd85a917906ba5f985f6f8
Darling Cabral <darling.cabral@willbank.com.br>
2023-02-10 10:08:20 -0300

    ci envs

--- Capybara/features/support/env.rb
+++ Capybara/features/support/env.rb
@@ -4,12 +4,11 @@ require 'site_prism'
 require 'rspec'
 
 Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app,
-  :browser => :chrome,
-  :chromeOptions => Selenium::WebDriver::Remote::Capabilities.chrome(
-    "goog:chromeOptions" => {"args" => [ "headless", "window-size=1280x720", "disable-gpu" ]})
-)
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
 
-Capybara.default_driver = :selenium
+caps = Selenium::WebDriver::Remote::Capabilities.chrome(
+    "goog:chromeOptions" => {"args" => [ "headless", "window-size=1280x720", "disable-gpu" ]})
+
+Capybara.default_driver = :selenium, desired_capabilities: caps
 Capybara.default_max_wait_time = 30
